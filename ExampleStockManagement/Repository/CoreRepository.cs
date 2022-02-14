81bbe20716231f7cdba024f9cb0cd7523fa25784
Kelvin Nielsen <kelvin@newtsdash.co.uk>
2022-02-14 13:10:46 +0100

    Add class to manage initialization order of repos.

---
+++ ExampleStockManagement/Repository/CoreRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleStockManagement.Repository
+{
+    class CoreRepository
+    {
+        private string fileName;
+        private WarehouseFileRepository warehouseFileRepository;
+        public WarehouseFileRepository WarehouseRepository
+        {
+            get { return warehouseFileRepository; }
+        }
+        private ItemFileRepository itemFileRepository;
+
+        public ItemFileRepository ItemRepository
+        {
+            get { return itemFileRepository; }
+        }
+        private OrderFileRepository orderFileRepository;
+
+        public OrderFileRepository OrderRepository
+        {
+            get { return orderFileRepository; }
+        }
+        private StockUnitFileRepository stockUnitFileRepository;
+
+        public StockUnitFileRepository StockUnitRepository
+        {
+            get { return stockUnitFileRepository; }
+        }
+        public CoreRepository(string fileName)
+        {
+            this.fileName = fileName;
+            warehouseFileRepository = new WarehouseFileRepository(fileName + "_warehouse.txt");
+            itemFileRepository = new ItemFileRepository(fileName + "_item.txt");
+            orderFileRepository = new OrderFileRepository(fileName + "_order.txt",warehouseFileRepository,itemFileRepository);
+            stockUnitFileRepository = new StockUnitFileRepository(fileName + "_stockUnit.txt",itemFileRepository,warehouseFileRepository);
+        }
+    }
+}
